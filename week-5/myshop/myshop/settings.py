33bea25eb50b76ee25d56f05e85fd68f728d7691
Sweet eyes <65070047@KMITL.AC.TH>
2024-08-01 17:28:05 +0700

    lab5

--- week-5/myshop/myshop/settings.py
+++ week-5/myshop/myshop/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'shop',
         'USER': 'postgres',
-        'PASSWORD': 'Boss3112545',
+        'PASSWORD': 'password',
         'HOST': 'localhost',
         'PORT': '5432',
     }
--- week-5/week5_tutorial/week5_tutorial/settings.py
+++ week-5/week5_tutorial/week5_tutorial/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
+    'companies',
+    'book',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'week5_tutorial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'week5',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
